ce72da52544d6a045c17da065b1189fac52da7fb
pakpak <pakpak@local>
2026-05-06 23:25:27 -0500

    Auto-save before branch switch

--- stable_flight.lua
+++ stable_flight.lua
@@ -5,8 +5,6 @@ for index, name in ipairs(peripheral.getNames()) do
     table.insert(peripherals, { index = index, name = name })
 end
 
-for i, name in ipairs(peripherals) do
-    print(i, name)
+for i, entry in ipairs(peripherals) do
+    print(i, entry.name)
 end
-
-print("TEST")
